2d8fd51d4618dd6390179c75ace308d8c2d62c80
carlosLGM <100099447+carlosLGM@users.noreply.github.com>
2022-07-11 18:52:19 -0600

    Update calculadora.js

--- ejercicio 3/js/calculadora.js
+++ ejercicio 3/js/calculadora.js
@@ -25,73 +25,69 @@ function init() {
     contenido += "</li>\n";
     contenido += "\t<li>\n";
     contenido += "\t\t<a href=\"javascript:void(0)\"><span>Restar</span></a>\n";
-    
-    
     contenido += "</li>\n";
-    contenido += "\t<li>\n";
-    contenido += "\t\t<a href=\"javascript:void(0)\"><span>Multiplicar</span></a>\n";
-    contenido += "</li>\n";
-    contenido += "\t<li>\n";
-    contenido += "\t\t<a href=\"javascript:void(0)\"><span>Dividir</span></a>\n";
-    contenido += "</li>\n";
-    contenido += "\t<li>\n";
-    contenido += "\t\t<a href=\"javascript:void(0)\"><span>Residuo</span></a>\n";
-    contenido += "</li>\n";
-    contenido += "</ul>\n";
-    contenido += "</nav>\n";
-    //Colocar el contenido dentro del elemento div
-    operaciones.innerHTML = contenido;
-    //Preparando el manejo del evento click sobre los enlaces del menú
-    var opciones = document.getElementsByTagName('a');
-    //Recorrer todos los elementos de enlace (elementos a)
-    //y asignar el manejador del evento click
-    for(var i=0; i<opciones.length; i++){
-    switch(i){
-    case 0:
-    //Función sumar
-    opciones[i].onclick = function(){
-    resultado.innerHTML = "<p class=\"letterpress\">" + op1 + " + " + op2 +
-    " = " + (parseFloat(op1) + parseFloat(op2)) + "</p>\n";
-    }
-    break;
-    case 1:
-    opciones[i].onclick = function(){
-    resultado.innerHTML = "<p class=\"letterpress\">" + op1 + " - " + op2 +
-    " = " + (op1 - op2) + "</p>\n";
-    }
-    break;
-    case 2:
-    opciones[i].onclick = function(){
-    resultado.innerHTML = "<p class=\"letterpress\">" + op1 + " * " + op2 +
-    " = " + (op1 * op2) + "</p>\n";
-    }
-    break;
-    case 3:
-    opciones[i].onclick = function(){
-    if(op2 == 0){
-    resultado.innerHTML = "<p class=\"letterpress\">No se puede dividirentre cero</p>";
-    }
-    else {
-    
-    resultado.innerHTML = "<p class=\"letterpress\">" + op1 + " / " +
-    op2 + " = " + Math.round((op1 / op2) * 100) / 100 + "</p>\n";
-    }
-    }
-    break;
-    case 4:
-    opciones[i].onclick = function(){
-    if(op2 == 0){
-    resultado.innerHTML = "<p class=\"letterpress\">No se ha podido obtener el residuo, por división entre cero.</p>\n";
-    }
-    else{
-    resultado.innerHTML = "<p class=\"letterpress\">" + op1 + " % " +
-    op2 + " = " + (op1 % op2) + "</p>\n";
-    }
-    }
-    break;
-    
-    }
-    
-    
-    }
-    }
+contenido += "\t<li>\n";
+contenido += "\t\t<a href=\"javascript:void(0)\"><span>Multiplicar</span></a>\n";
+contenido += "</li>\n";
+contenido += "\t<li>\n";
+contenido += "\t\t<a href=\"javascript:void(0)\"><span>Dividir</span></a>\n";
+contenido += "</li>\n";
+contenido += "\t<li>\n";
+contenido += "\t\t<a href=\"javascript:void(0)\"><span>Residuo</span></a>\n";
+contenido += "</li>\n";
+contenido += "</ul>\n";
+contenido += "</nav>\n";
+//Colocar el contenido dentro del elemento div
+operaciones.innerHTML = contenido;
+//Preparando el manejo del evento click sobre los enlaces del menú
+var opciones = document.getElementsByTagName('a');
+//Recorrer todos los elementos de enlace (elementos a)
+//y asignar el manejador del evento click
+for(var i=0; i<opciones.length; i++){
+switch(i){
+case 0:
+//Función sumar
+opciones[i].onclick = function(){
+resultado.innerHTML = "<p class=\"letterpress\">" + op1 + " + " + op2 +
+" = " + (parseFloat(op1) + parseFloat(op2)) + "</p>\n";
+}
+break;
+case 1:
+opciones[i].onclick = function(){
+resultado.innerHTML = "<p class=\"letterpress\">" + op1 + " - " + op2 +
+" = " + (op1 - op2) + "</p>\n";
+}
+break;
+case 2:
+opciones[i].onclick = function(){
+resultado.innerHTML = "<p class=\"letterpress\">" + op1 + " * " + op2 +
+" = " + (op1 * op2) + "</p>\n";
+}
+break;
+case 3:
+opciones[i].onclick = function(){
+if(op2 == 0){
+resultado.innerHTML = "<p class=\"letterpress\">No se puede dividir entre cero</p>";
+}
+else {
+
+resultado.innerHTML = "<p class=\"letterpress\">" + op1 + " / " +
+op2 + " = " + Math.round((op1 / op2) * 100) / 100 + "</p>\n";
+}
+}
+break;
+case 4:
+opciones[i].onclick = function(){
+if(op2 == 0){
+resultado.innerHTML = "<p class=\"letterpress\">No se ha podido obtener el residuo, por división entre cero.</p>\n";
+}
+else{
+resultado.innerHTML = "<p class=\"letterpress\">" + op1 + " % " +
+op2 + " = " + (op1 % op2) + "</p>\n";
+}
+}
+break;
+
+}
+}
+}
